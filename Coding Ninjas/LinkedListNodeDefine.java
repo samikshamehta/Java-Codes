782b54284ab97fe64f94062abcd45c80a70b5e38
samikshamehta <36148231+samikshamehta@users.noreply.github.com>
2019-05-12 11:59:08 +0530

    Add files via upload

---
+++ Coding Ninjas/LinkedListNodeDefine.java
@@ -0,0 +1,38 @@
+class LinkedListNode<T> {
+	T data;
+	LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.data = data;
+	}
+}
+class ListNode<T> {
+	T data;
+	ListNode<T> next;
+
+	public ListNode(T data) {
+		this.data = data;
+	}
+}
+class Node<T>
+{
+	T data;
+	Node<T> next;
+	
+	Node(T data)
+	{
+		this.data=data;
+	}
+}
+
+public class LinkedListNodeDefine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
